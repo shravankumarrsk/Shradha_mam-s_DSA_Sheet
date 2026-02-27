65ab9a9a8d3bebdbed10475920b79fdcf5f60905
shravankumarrsk <shravankumarrsk>
2026-02-27 12:17:38 +0530

    Day 1

--- Arrays/day1.cpp
+++ Arrays/day1.cpp
@@ -8,7 +8,8 @@ Example 1:
 
 Input: nums = [3,2,3]
 Output: 3   */
-
+#include<bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
@@ -24,3 +25,86 @@ public:
         return -1;
     }
 };
+
+
+/*Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,1]
+
+Output: 1   */
+
+//bruteforce approach
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>mpp;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+        }
+        for(auto it:mpp){
+            if(it.second==1){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
+
+
+
+//optimal approach
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        int result=0;
+        for(int i=0;i<n;i++){
+                result^=nums[i];
+        }
+        return result;
+    }
+};
+
+
+
+
+/* You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+   */
+#include<bits/stdc++.h>
+using namespace std;
+class solutoion{
+    public:
+    int maxprofit(vector<int>&prices){
+        int n=prices.size();
+        int profit=0;
+        int minprice=INT_MAX;
+        for(int i=0;i<n;i++){
+            minprice=min(minprice,prices[i]);
+            profit=max(profit,prices[i]-minprice);
+        }
+        return profit;
+    }
+};
+
