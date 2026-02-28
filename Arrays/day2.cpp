0d1dead787aba33ce4c9a256c102844eee7acbd7
shravankumarrsk <shravankumarrsk>
2026-03-01 00:51:51 +0530

    streak

--- Arrays/day2.cpp
+++ Arrays/day2.cpp
@@ -1,7 +1,4 @@
 /*Given an integer array nums, find the subarray with the largest sum, and return its sum.
-
- 
-
 Example 1:
 
 Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
@@ -31,3 +28,71 @@ public:
         return maxi;
     }
 };
+
+
+
+/* Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+ 
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]   */
+
+
+//brute for(auto var : collection_to_loop)
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+       int n=nums.size();
+       int i=1;
+       while(i<n){
+        if(nums[i]<nums[i-1]){
+            swap(nums[i],nums[i-1]);
+            i=1;
+        }else{
+            i++;
+        }
+       }
+       
+    }
+};
+
+//optimal approach (dutch national flag algorithm)
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+       int n=nums.size();
+       int low=0;int mid=0;int high=n-1;
+       while(mid<=high){
+        if(nums[mid]==0){
+            swap(nums[low],nums[mid]);
+            low++;
+            mid++;
+        }
+        else if(nums[mid]==1){
+            mid++;
+        }
+        else{
+            swap(nums[mid],nums[high]);
+            high--;
+        }
+       }
+       return ;
+    }
+};
+
+
+
+/* 
+
+The next permutation  question is not yet solved by me. when it is solved the sheet will be updated.
+
+*/
